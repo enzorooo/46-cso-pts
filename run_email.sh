3f9611f86f2d21a821fdd216c89807f399c0dba5
Lorenzo Mercado <ph.enzomercado@gmail.com>
2021-05-10 10:27:27 +0000

    modified shall executable

--- run_email.sh
+++ run_email.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-<<<<<<< HEAD
-python3 opa_email/opa_email.py
-=======
-python3 opa_email/opa_email.py
->>>>>>> 44f1938c856043379bdbd361dfd64c1b3a7a0f88
+python3 /home/juan_lorenzo_mercado/46-cso-pts/opa_email/opa_email.py
